5568e23c7b3fc35e1d31f998879cee56c5187592
Gordon <gordon@Gordons-MacBook-Air.local>
2019-10-08 17:09:23 -0400

    primitive types, operators

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
+
+            //Primitive Types
             const float number = 1.0f;
             int output = (int)number;
             Console.WriteLine(output);
@@ -30,10 +32,10 @@ namespace HelloWorld
             {
                 Console.WriteLine("The number could not be converted to a byte");
             }
-            
 
-            
-            
+
+
+
             //int i = 1000;
             //byte b = (byte)i;
             //string s = "1";
@@ -42,9 +44,24 @@ namespace HelloWorld
             //int output2 = int.Parse(s);
             //char output3 = (char)s1;
             //Console.WriteLine(output1);
-            //Console.WriteLine("parse uncompatible types {0}", output2);
+            //Console.WriteLine("parse imcompatible types {0}", output2);
             //Console.WriteLine(output3);
             //Console.WriteLine(b);
+
+            //Operators
+            //Arithmetic Operators
+            var firstnum = 10;
+            var secondnum = 3;
+            Console.WriteLine((float)firstnum / (float)secondnum);
+            //Comparison Operators
+            var num1 = 10;
+            var num2 = 3;
+            var num3 = 11;
+            Console.WriteLine(num2 == num1 || num3 > num1); //True
+            Console.WriteLine(num1 == num2); //Flase
+            Console.WriteLine(num1 > num2); //True
+            Console.WriteLine(num1 != num2); //True
+            Console.WriteLine(num3 > num1 && num3 > num2); //True
         }
     }
 }
