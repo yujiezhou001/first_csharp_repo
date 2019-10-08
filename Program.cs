b3774f3c24d950502ab26a6764d49187a20a8575
Gordon <gordon@Gordons-MacBook-Air.local>
2019-10-08 16:32:11 -0400

    casting, type conversion between incompatible types, and exception

--- Program.cs
+++ Program.cs
@@ -9,15 +9,42 @@ namespace HelloWorld
             const float number = 1.0f;
             int output = (int)number;
             Console.WriteLine(output);
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The string could not be converted to a boolean");
+            }
+            try
+            {
+                string p = "1234";
+                byte p1 = Convert.ToByte(p);
+                Console.WriteLine(p1);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
+            
 
-            string s = "1";
-            int s1 = 1;
-            int output1 = Convert.ToInt32(s);
-            int output2 = int.Parse(s);
-            char output3 = (char)s1;
-            Console.WriteLine(output1);
-            Console.WriteLine(output2);
-            Console.WriteLine(output3);
+            
+            
+            //int i = 1000;
+            //byte b = (byte)i;
+            //string s = "1";
+            //int s1 = 1;
+            //int output1 = Convert.ToInt32(s);
+            //int output2 = int.Parse(s);
+            //char output3 = (char)s1;
+            //Console.WriteLine(output1);
+            //Console.WriteLine("parse uncompatible types {0}", output2);
+            //Console.WriteLine(output3);
+            //Console.WriteLine(b);
         }
     }
 }
