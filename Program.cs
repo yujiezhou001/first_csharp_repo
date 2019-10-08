80fd53dbc1a44532594075e8fbb5d3c946485f3b
Gordon <gordon@Gordons-MacBook-Air.local>
2019-10-08 11:55:50 -0400

    conversion of types

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const float number = 1.0f;
+            int output = (int)number;
+            Console.WriteLine(output);
+
+            string s = "1";
+            int s1 = 1;
+            int output1 = Convert.ToInt32(s);
+            int output2 = int.Parse(s);
+            char output3 = (char)s1;
+            Console.WriteLine(output1);
+            Console.WriteLine(output2);
+            Console.WriteLine(output3);
+        }
+    }
+}
