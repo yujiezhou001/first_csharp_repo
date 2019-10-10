765858875a5557b068606ab7bf523614de173dbd
yujiezhou001 <47671130+yujiezhou001@users.noreply.github.com>
2019-10-10 15:55:32 -0400

    add another attribute (age)

--- Person.cs
+++ Person.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 namespace NonPrimitives
 {
     public class Person
     {
         public string FirstName;
         public string LastName;
+        public int Age;
         public void introduce()
         {
             Console.WriteLine("My name is " + FirstName + " " + LastName);
