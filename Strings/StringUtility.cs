d6577f7741e292316b0fe19c9625d7aeda334fdc
yujiezhou001 <47671130+yujiezhou001@users.noreply.github.com>
2019-10-19 21:30:57 -0400

    created StringUtility class
    This class is called in Program.cs, which contains a function that summarizes text.

---
+++ Strings/StringUtility.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace Strings
+{
+    public class StringUtility
+    {
+        public static string SummerizeTest(string text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+                return text;
+
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                    break;
+            }
+
+            return String.Join(" ", summaryWords) + "...";
+        }
+    }
+}
